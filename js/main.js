d823ff4ecf81fc6c9b9baadafb94b80ffc075698
Farwa Syed <farwa.asyed@gmail.com>
2019-02-16 22:34:03 +1100

    began

---
+++ js/main.js
@@ -0,0 +1,7 @@
+console.log("up and running");
+var cardOne = "queen1";
+var cardTwo = "queen2";
+var cardThree = "king1";
+var cardFour = "king2";
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardThree);
