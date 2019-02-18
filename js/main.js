9b526ea9a11966b88f2ca4297cd58547ad2bcd2b
Farwa Syed <farwa.asyed@gmail.com>
2019-02-19 00:07:54 +1100

    game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
 console.log("up and running");
-var cardOne = "queen1";
-var cardTwo = "queen2";
-var cardThree = "king1";
-var cardFour = "king2";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+// var cardOne = "queen1";
+// var cardTwo = "queen2";
+// var cardThree = "king1";
+// var cardFour = "king2";
+// console.log("User flipped" + cardOne);
+// console.log("User flipped" + cardThree);
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+  if(cardsInPlay[0] === cardsInPlay[1]){
+    console.log("you found match");
+  } else {
+    console.log("try again");
+  }
+} else {
+  console.log("nop");
+}
